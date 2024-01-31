9bc12d32b03a4eca7c855d0bb6ef9a459dc47997
Alvio Leegia <exralvio@gmail.com>
2024-02-01 02:17:17 +0700

    Update delivery modell

--- backend/delivery/models.py
+++ backend/delivery/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 from common.models import DisplayData
+from order.models import OrderItem
 
 
 class Delivery(DisplayData):
     id = models.BigAutoField(auto_created=True, primary_key=True)
-    order_item_id = models.IntegerField(null=True)
+    order_item = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
     delivered_quantity = models.IntegerField(null=True)
 
     class Meta:
